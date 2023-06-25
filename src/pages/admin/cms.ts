042f7723b6d78bfc0beb7a49cc8d0c7779a391c4
pavro_roman <roman.pavlov.1996@bk.ru>
2023-06-25 22:31:42 +0300

    Подключил NetlifyCMS

---
+++ src/pages/admin/cms.ts
@@ -0,0 +1,28 @@
+import CMS from 'netlify-cms-app';
+import GlobalStyles from '../../styles/base/base.css?inline';
+import collections from '../../collections';
+
+console.log('okey')
+CMS.init({
+  config: {
+    backend: {
+      name: 'git-gateway',
+      branch: 'latest',
+    },
+    local_backend: true,
+    media_folder: 'public/assets/blog',
+    public_folder: 'assets/blog',
+    collections: collections.map(({ collection }) => collection),
+  },
+});
+
+/**
+ * Another drawback of using Netlify CMS is that it registers all preview
+ * styles globally — not scoped to a specific collection.
+ * You’ve lost Astro component’s scoped styling anyway by being forced
+ * to use React, but just be extra careful.
+ *
+ * The (undocumented?) `raw: true` setting treats the first argument as
+ * a raw CSS string to inject instead of as a URL to load a stylesheet from.
+ */
+CMS.registerPreviewStyle(GlobalStyles, { raw: true });
