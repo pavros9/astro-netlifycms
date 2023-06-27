c828895188f8b5a58a1a5ddad23165249d4e4037
pavro_roman <roman.pavlov.1996@bk.ru>
2023-06-27 13:38:18 +0300

    Добавил Слайдер на страницу

---
+++ src/components/ReactComponents/SliderTemplate/SliderTemplate.tsx
@@ -0,0 +1,20 @@
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import './SliderTemplate.css'
+
+
+export const SliderTemplate = () => {
+
+    return <Swiper className="mySwiper">
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+            <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide>
+  </Swiper>
+}
