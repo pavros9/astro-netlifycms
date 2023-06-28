278fdb09dbee7ef4298931d0a5a8e6e5542adf92
pavro_roman <roman.pavlov.1996@bk.ru>
2023-06-28 13:46:14 +0300

    Пофиксил загрузку скрипта

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-
   integrations: [
     tailwind(),
     react(),
---
+++ src/pages/admin/Admin.tsx
@@ -0,0 +1,34 @@
+import CMS   from 'netlify-cms-app';
+import GlobalStyles from '../../styles/base/base.css?inline';
+import collections from '../../collections';
+import { PagePreview } from '../../components/Preview/Page'
+import { NavigationPreview } from '../../components/Preview/Navigation'
+import { useEffect} from 'react'
+
+
+const Admin = () => {
+    useEffect(() => {
+        CMS.init({
+            config: {
+                locale: 'ru',
+                backend: {
+                name: 'git-gateway',
+                branch: 'latest',
+                },
+                local_backend: true,
+                media_folder: 'public/assets/blog',
+                public_folder: 'assets/blog',
+                collections: collections.map(({ collection }) => collection),
+            },
+            })
+            
+            
+            CMS.registerPreviewStyle(GlobalStyles, { raw: true });
+            
+            CMS.registerPreviewTemplate("pages", PagePreview);
+            CMS.registerPreviewTemplate("site", NavigationPreview);
+    }, [])
+    return <></>
+}
+
+export default Admin;
